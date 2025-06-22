5907e71f00b71156aa4b5302975747868e9194c0
alayyafaza21 <alayyalayaali@gmail.com>
2025-06-22 22:36:57 +0700

    Add files via upload

---
+++ Alayya Faza Layaali_245150407111043/databasefeedback.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $db_name = "edukids";       
+    private $username = "root";         
+    private $password = "";            
+    public $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Koneksi database gagal: " . $exception->getMessage();
+            exit();
+        }
+
+        return $this->conn;
+    }
+}
